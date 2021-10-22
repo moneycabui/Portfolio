8c732e10584d6fcda415195a934ab77e66c34bc8
Monica Bui <monicabbui@gmail.com>
2021-10-21 17:44:04 -0700

    Change order of pictures

--- client/src/components/Home/images.js
+++ client/src/components/Home/images.js
@@ -1,11 +1,11 @@
 const images = [
   // Array format: [src, alt];
   ['/images/interests/CaboPool.jpeg', 'Cabo'],
-  ['/images/interests/SelfPortrait.jpg', 'Self Portrait'],
   ['/images/interests/YosemiteBackpacking.jpg', 'Backpacking in Yosemite'],
   ['/images/interests/GlacierPoint.jpg', 'Glacier Point'],
   ['/images/interests/YosemiteHammock.jpg', 'Hammocking in Yosemite'],
   ['/images/interests/BackpackingFood.jpg', 'Enjoying backpacking food at Glacier Point'],
+  ['/images/interests/SelfPortrait.jpg', 'Self Portrait'],
   ['/images/interests/Canyon.jpeg', 'Canyon in Arizona'],
   ['/images/interests/LakePowell.jpeg', 'Lake Powell'],
   ['/images/interests/BobaStitch.jpeg', 'Boba Stitch']
